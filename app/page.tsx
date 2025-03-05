4d40714959c7e0216345f6d1360b199a5bac8f86
AlketKuehne <alketrrushi0@gmail.com>
2025-03-05 14:43:06 +0000

    Logo try 2

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,7 @@ export default function Home() {
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
         <Image
           className="dark:invert"
-          src="/next.svg"
+          src="C:\Users\ext.Rrushi.Alket\OneDrive - Kuehne+Nagel\Desktop\Next.js-Projekt\public\screenshot.jpg"
           alt="Next.js logo"
           width={180}
           height={38}
