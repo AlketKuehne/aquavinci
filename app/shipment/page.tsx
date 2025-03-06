6cba2c984e7112ed873bf8397bef54cfec6ed8bd
AlketKuehne <alketrrushi0@gmail.com>
2025-03-06 15:28:52 +0000

    button on bar

--- app/shipment/page.tsx
+++ app/shipment/page.tsx
@@ -7,23 +7,29 @@ export default function ShipmentPage() {
       
       {/* Navigation Bar */}
       <nav className="w-full h-12 bg-[#242424] flex items-center px-4">
-        {/* Klickbares Logo für die Homepage */}
+        {/* Clickable Logo for Homepage */}
         <Link href="/">
           <Image src="/logoname.png" alt="Logo" width={150} height={80} className="h-12 w-auto cursor-pointer" />
         </Link>
 
-        {/* Navigation Links (direkt nach dem Logo) */}
-        <div className="flex h-full ml-4"> {/* Hier wurde `ml-4` hinzugefügt */}
-          <Link href="/" className="flex items-center justify-center px-6 text-lg text-white hover:bg-gray-600 h-full">
+        {/* Navigation Links (Buttons start right after logo) */}
+        <div className="flex h-full ml-4">
+          <Link
+            href="/"
+            className="flex items-center justify-center px-6 text-lg text-white bg-[#242424] transition-all duration-1000 hover:bg-gray-400 hover:text-black h-full"
+          >
             Homepage
           </Link>
-          <Link href="/shipment" className="flex items-center justify-center px-6 text-lg text-white hover:bg-gray-600 h-full">
+          <Link
+            href="/shipment"
+            className="flex items-center justify-center px-6 text-lg text-white bg-[#242424] transition-all duration-1000 hover:bg-gray-400 hover:text-black h-full"
+          >
             Create Shipment
           </Link>
         </div>
       </nav>
 
-      {/* Seiteninhalt */}
+      {/* Page Content */}
       <div className="flex flex-col items-center justify-center flex-grow p-8">
         <h1 className="text-3xl font-bold">Shipment Page</h1>
         <p className="mt-4 text-lg">Welcome to the shipment page!</p>
