c495d3570b5b872f985846079d937c646ba2db66
AlketKuehne <alketrrushi0@gmail.com>
2025-03-06 15:15:25 +0000

    bar got edited 3

--- app/shipment/page.tsx
+++ app/shipment/page.tsx
@@ -1,17 +1,24 @@
 import Link from "next/link";
+import Image from "next/image";
 
 export default function ShipmentPage() {
   return (
     <div className="flex flex-col items-center min-h-screen">
       
-      {/* Dünne, durchgehende Navigationsleiste */}
-      <nav className="w-full h-6 bg-[#242424] flex">
-        <Link href="/" className="flex items-center px-4 text-sm text-white hover:bg-gray-600 w-40">
-          Homepage
-        </Link>
-        <Link href="/shipment" className="flex items-center px-4 text-sm text-white hover:bg-gray-600 w-40">
-          Create Shipment
-        </Link>
+      {/* Navigation Bar */}
+      <nav className="w-full h-12 bg-[#242424] flex items-center px-4">
+        {/* Logo (ganz links) */}
+        <Image src="/logoname.png" alt="Logo" width={100} height={40} className="h-10 w-auto" />
+
+        {/* Navigation Links */}
+        <div className="flex ml-8">
+          <Link href="/" className="flex items-center px-6 text-sm text-white hover:bg-gray-600 h-full">
+            Homepage
+          </Link>
+          <Link href="/shipment" className="flex items-center px-6 text-sm text-white hover:bg-gray-600 h-full">
+            Create Shipment
+          </Link>
+        </div>
       </nav>
 
       {/* Seiteninhalt */}
