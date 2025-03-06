b183f7e87e4b2e44c26ff104446eb6a886de0471
AlketKuehne <alketrrushi0@gmail.com>
2025-03-06 15:19:06 +0000

    edited the bar

--- app/shipment/page.tsx
+++ app/shipment/page.tsx
@@ -6,16 +6,18 @@ export default function ShipmentPage() {
     <div className="flex flex-col items-center min-h-screen">
       
       {/* Navigation Bar */}
-      <nav className="w-full h-12 bg-[#242424] flex items-center px-4">
-        {/* Logo (ganz links) */}
-        <Image src="/logoname.png" alt="Logo" width={100} height={40} className="h-10 w-auto" />
+      <nav className="w-full h-16 bg-[#242424] flex items-center px-4">
+        {/* Klickbares Logo für die Homepage */}
+        <Link href="/">
+          <Image src="/logoname.png" alt="Logo" width={120} height={64} className="h-16 w-auto cursor-pointer" />
+        </Link>
 
         {/* Navigation Links */}
-        <div className="flex ml-8">
-          <Link href="/" className="flex items-center px-6 text-sm text-white hover:bg-gray-600 h-full">
+        <div className="flex ml-8 h-full">
+          <Link href="/" className="flex items-center justify-center px-6 text-lg text-white hover:bg-gray-600 h-full">
             Homepage
           </Link>
-          <Link href="/shipment" className="flex items-center px-6 text-sm text-white hover:bg-gray-600 h-full">
+          <Link href="/shipment" className="flex items-center justify-center px-6 text-lg text-white hover:bg-gray-600 h-full">
             Create Shipment
           </Link>
         </div>
