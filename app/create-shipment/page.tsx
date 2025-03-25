affe6ec8c2475939a8cfb05a80bbc9a8ed2bb59d
AlketKuehne <alketrrushi0@gmail.com>
2025-03-25 13:31:46 +0000

    edited background popup

--- app/create-shipment/page.tsx
+++ app/create-shipment/page.tsx
@@ -372,7 +372,6 @@ export default function ShipmentPage() {
         </div>
         {showWarning && (
           <div className="fixed top-0 left-0 w-full h-full flex items-center justify-center">
-            <div className="absolute top-0 left-0 w-full h-full bg-gray-500 bg-opacity-50"></div>
             <div className="bg-white p-6 rounded-lg shadow-lg relative w-96 z-10">
               <button
                 className="absolute top-2 right-2 text-gray-500 hover:text-gray-700"
