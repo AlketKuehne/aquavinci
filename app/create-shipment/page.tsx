454c3d93420f3da22a3e20932aee008ce97948e9
AlketKuehne <alketrrushi0@gmail.com>
2025-03-25 12:13:31 +0000

    edited path to subsubpage

--- app/create-shipment/page.tsx
+++ app/create-shipment/page.tsx
@@ -163,7 +163,7 @@ export default function ShipmentPage() {
 
         {/* Continue Button */}
         <div className="flex justify-end w-full mt-16 mb-16">
-          <Link href="/shipment/nextPage">
+          <Link href="/create-shipment/nextPage">
             <button className="flex items-center px-6 py-3 bg-black text-white text-lg font-medium rounded-full transition-all duration-[1250ms] hover:bg-[#E5E5E5] hover:text-black">
               Continue
             </button>
