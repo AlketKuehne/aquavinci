db04829d954159d5e72d6e6d9bf45b5d813620f9
AlketKuehne <alketrrushi0@gmail.com>
2025-03-25 13:59:27 +0000

    edited background of popup

--- app/create-shipment/page.tsx
+++ app/create-shipment/page.tsx
@@ -374,7 +374,7 @@ export default function ShipmentPage() {
           </button>
         </div>
         {showWarning && (
-          <div className="fixed top-0 left-0 w-full h-full flex items-center justify-center" style={{ backgroundColor: 'rgba(128, 128, 128, 0.5)' }}>
+          <div className="fixed top-0 left-0 w-full h-full flex items-center justify-center" style={{ backgroundColor: 'rgba(0, 0, 0, 0.5)' }}>
             <div className="bg-white p-6 rounded-lg shadow-lg relative w-96 z-10">
               <button
                 className="absolute top-2 right-2 text-gray-500 hover:text-gray-700"
