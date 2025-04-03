986c2487715dd2bd8f96bc955c819b431ae8e952
AlketKuehne <alketrrushi0@gmail.com>
2025-04-03 08:06:09 +0000

    edited details

--- app/create-shipment/details/Popup.tsx
+++ app/create-shipment/details/Popup.tsx
@@ -5,15 +5,15 @@ export default function Popup({ onStay, onLeave }: { onStay: () => void; onLeave
     <div className="fixed top-0 left-0 w-full h-full bg-black bg-opacity-50 z-50 flex items-center justify-center pointer-events-auto">
       <div className="bg-white p-8 rounded-lg shadow-lg w-96 relative text-center z-10">
         <h2 className="text-lg font-bold mb-6">Are you certain you wish to leave this page? Unsaved changes will be lost.</h2>
-        <div className="flex justify-center gap-8 mt-6">
+        <div className="flex justify-center gap-10 mt-6">
           <button
-            className="px-6 py-3 bg-gray-300 text-black text-base rounded-full transition-all duration-[1250ms] hover:bg-black hover:text-white cursor-pointer"
+            className="px-8 py-4 bg-gray-300 text-black text-base rounded-full transition-all duration-[1250ms] hover:bg-black hover:text-white cursor-pointer"
             onClick={onLeave} // Ensure this calls the onLeave function to leave the page
           >
             Yes
           </button>
           <button
-            className="px-6 py-3 bg-black text-white text-base rounded-full transition-all duration-[1250ms] hover:bg-gray-300 hover:text-black cursor-pointer"
+            className="px-8 py-4 bg-black text-white text-base rounded-full transition-all duration-[1250ms] hover:bg-gray-300 hover:text-black cursor-pointer"
             onClick={onStay} // Ensure this calls the onStay function to stay on the page
           >
             No
