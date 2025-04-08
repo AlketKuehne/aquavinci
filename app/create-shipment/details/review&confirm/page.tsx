5269fc2aaf9810895c6cf7f96b02cbc0da106d5b
AlketKuehne <alketrrushi0@gmail.com>
2025-04-08 12:18:22 +0000

    edited structure

--- app/create-shipment/details/review&confirm/page.tsx
+++ app/create-shipment/details/review&confirm/page.tsx
@@ -31,10 +31,10 @@ export default function ReviewAndConfirmPage() {
   };
 
   return (
-    <div className="flex flex-col items-start w-full max-w-6xl mt-4 px-8 pt-12">
+    <div className="flex flex-col items-center w-full max-w-6xl mt-4 px-8 pt-12">
       <NavigationBar onNavigate={handleNavigate} />
       <h1 className="text-4xl font-extrabold mb-8 self-start">Review & Confirm</h1>
-      <div className="grid grid-cols-2 gap-x-4 gap-y-7 w-full">
+      <div className="grid grid-cols-2 gap-x-6 gap-y-8 w-full">
         {/* Shipment Type */}
         <div className="bg-white p-6 shadow-lg rounded-lg flex justify-between items-center">
           <div>
