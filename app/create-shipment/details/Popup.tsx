eea7770d3a827e36f9616d9f348b2a0c63f6d046
AlketKuehne <alketrrushi0@gmail.com>
2025-04-08 11:36:54 +0000

    edited other popup

--- app/create-shipment/details/Popup.tsx
+++ app/create-shipment/details/Popup.tsx
@@ -7,7 +7,7 @@ export default function Popup({ onStay, onLeave }: { onStay: () => void; onLeave
       className="fixed top-0 left-0 w-full h-full z-50 flex items-center justify-center pointer-events-auto"
     >
       <div className="bg-white p-8 rounded-lg shadow-lg w-96 relative text-center z-10">
-        <h2 className="text-lg font-bold mb-6">Are you certain you wish to leave this page? Unsaved changes will be lost.</h2>
+        <h2 className="text-lg font-bold mb-6">You have unsaved changes. Are you sure you want to leave this page?</h2>
         <div className="flex justify-center gap-10 mt-6">
           <button
             className="px-8 py-4 bg-gray-300 text-black text-base rounded-full transition-all duration-[1250ms] hover:bg-black hover:text-white cursor-pointer"
