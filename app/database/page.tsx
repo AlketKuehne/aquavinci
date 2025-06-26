f100efa118cf3f6977586798988f10b82dd594b4
AlketKuehne <alketrrushi0@gmail.com>
2025-06-26 07:22:24 +0000

    fixed issue

--- app/database/page.tsx
+++ app/database/page.tsx
@@ -492,7 +492,7 @@ export default function DatabasePage() {
               </motion.div>
             )}
           </div>
-          <table className="min-w-full border ml-16 mt-2" style={{ borderCollapse: 'separate', borderSpacing: 0 }}>
+          <table className="min-w-full" style={{ borderCollapse: 'separate', borderSpacing: 0 }}>
             <thead>
               <tr className="bg-[#F5F5F5]">
                 <th className="border px-4 py-2 rounded-tl-xl">Edit</th>
